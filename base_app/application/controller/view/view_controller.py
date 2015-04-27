e429ddc11c2157b52d55dff5255d9654a6540cd6
wilsonaero <nwilson.aero@gmail.com>
2015-04-27 06:06:47 -0400

    Basic integration of MDIArea in place of QTabWidget.  Still a few issues... Need to catch the window maximize event in order to switch back to TabbedView if TabbedView not desired for tiled windows.  If tabbed view preferred always... need to catch height of tabbed area to improve behavior of custom tiling.

--- base_app/application/controller/view/view_controller.py
+++ base_app/application/controller/view/view_controller.py
@@ -41,7 +41,12 @@ class BaseAppViewController(object):
         self._view.action_file_settings_plugins.triggered.connect(self._file_settings_plugins)
         self._view.action_file_exit.triggered.connect(self._file_exit)
 
-        self._view.tab_widget.currentChanged.connect(self._update_current_tab)
+        self._view.action_window_htile.triggered.connect(self._window_horz_tile)
+        self._view.action_window_vtile.triggered.connect(self._window_vert_tile)
+        self._view.action_window_cascade.triggered.connect(self._window_cascade)
+
+        #todo: remove for MDI
+        #self._view.tab_widget.currentChanged.connect(self._update_current_tab)
 
     def show(self):
         self._view.show()
@@ -64,11 +69,36 @@ class BaseAppViewController(object):
             return
         self._is_active = False
 
-        new_tab = QtGui.QWidget()
-        new_tab.setObjectName(_new_name)
-        new_tab.grid_layout = QtGui.QGridLayout(new_tab)
+        #todo: remove  deprecated for MDI
+        # new_tab = QtGui.QWidget()
+        # new_tab.setObjectName(_new_name)
+        # new_tab.grid_layout = QtGui.QGridLayout(new_tab)
+        # self._view.add_tab(new_tab, _new_name)
 
-        self._view.add_tab(new_tab, _new_name)
+
+        # MDI
+        new_subwindow = QtGui.QMdiSubWindow()
+        new_subwindow.setWindowTitle(_new_name)
+        # looks like will have to subclass the subwindow to install an event filter to handle window maximize
+        #new_subwindow.windowStateChanged.connect(lambda: self.window_state(new_subwindow))
+        self._view.add_subwindow(new_subwindow)
+
+    def window_state(self,wdw):
+
+        # on window state change... if wdw is maximized, switch mdiarea to subwindow view
+        # may have to be done by subclasssing mdisubwindow
+        #
+        print("running")
+        print("{0}".format(str(wdw.windowTitle())))
+        print("{0}".format(str(wdw.windowState())))
+
+        # if wdw.windowState() & QtCore.Qt.WindowMaximized:
+        #     self._view.mdiarea.setViewMode(QtGui.QMdiArea.TabbedView)
+        #     print("maximized: {0}".format(str(wdw.windowTitle())))
+        # else:
+        #     self._view.mdiarea.setViewMode(QtGui.QMdiArea.SubWindowView)
+        #     print("minimized: {0}".format(str(wdw.windowTitle())))
+
 
     def close_document(self):
 
@@ -131,3 +161,76 @@ class BaseAppViewController(object):
 
     def _file_exit(self):
         print 'file exit'
+
+    def _window_horz_tile(self):
+
+        # todo:  investigate: some mdiarea attribute is adjusted on cascadeSubWindows and tileSubWindows
+        #   currently execute cascadeSubWindows first before custom tiling ... if not, I miss some mdiarea attribute and doesn't display properly
+        self._view.mdiarea.cascadeSubWindows()
+
+        position = QtCore.QPoint(0,0)
+
+        for wdw in self._view.mdiarea.subWindowList():
+
+            new_width = self._view.mdiarea.size().width() / (len(self._view.mdiarea.subWindowList()))
+            new_height = self._view.mdiarea.size().height()
+
+            #todo:  new_height includes tabbed height... need it to be height of mdi content area
+
+            #print("width = {0} / height = {1}".format(self._view.mdiarea.size().width(),self._view.mdiarea.size().height()))
+            #print("new_width = {0} / new_height = {1}".format(new_width,new_height))
+
+            # Note:  setGeometry does not override wdw Minimum Size.... have to reset Minimum Size
+            wdw.setMinimumSize(0,0)
+            rect = QtCore.QRect(0,0,new_width,new_height)
+            wdw.setGeometry(rect)
+            #print("wdw.w = {0} / wdw.h = {1}".format(wdw.width(),wdw.height()))
+
+            wdw.move(position)
+            position.setX(position.x() + wdw.width())
+
+    def _window_vert_tile(self):
+
+        # todo:  investigate: some mdiarea attribute is adjusted on cascadeSubWindows and tileSubWindows
+        #   currently execute cascadeSubWindows first before custom tiling ... if not, I miss some mdiarea attribute and doesn't display properly
+        self._view.mdiarea.cascadeSubWindows()
+
+
+        # Note:  subwindow maximize event needs to change mdiarea to tabbed view
+        #self._view.mdiarea.setViewMode(QtGui.QMdiArea.SubWindowView)
+
+        position = QtCore.QPoint(0,0)
+
+        for wdw in self._view.mdiarea.subWindowList():
+
+            new_width = self._view.mdiarea.size().width()
+            new_height = self._view.mdiarea.size().height() / (len(self._view.mdiarea.subWindowList()))
+
+            #todo:  new_height includes tabbed height... need it to be height of mdi content area
+            #   --- IF staying in TabbedView Mode
+            #   --- ELSE... over-ride the tab maximize event to switch back to tabbed view
+
+
+
+            #print("width = {0} / height = {1}".format(self._view.mdiarea.size().width(),self._view.mdiarea.size().height()))
+            #print("new_width = {0} / new_height = {1}".format(new_width,new_height))
+
+            # Note:  setGeometry does not override wdw Minimum Size.... have to reset Minimum Size
+            wdw.setMinimumSize(0,0)
+            rect = QtCore.QRect(0,0,new_width,new_height)
+            wdw.setGeometry(rect)
+            #print("wdw.w = {0} / wdw.h = {1}".format(wdw.width(),wdw.height()))
+
+            wdw.move(position)
+            position.setY(position.y() + wdw.height())
+
+
+    def _window_cascade(self):
+
+        # Set cascading window size related to mdiarea size
+        w_min = self._view.mdiarea.size().width() * 0.6
+        h_min = self._view.mdiarea.size().height() * 0.6
+
+        for wdw in self._view.mdiarea.subWindowList():
+            wdw.setMinimumSize(w_min,h_min)
+        self._view.mdiarea.cascadeSubWindows()
